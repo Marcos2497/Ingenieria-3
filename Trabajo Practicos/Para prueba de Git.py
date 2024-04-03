348346930cf4d40bb0861fbf91ab529a56c81f51
Marcos2497 <marcosnatad@gmail.com>
2024-04-03 15:33:57 -0300

    Agregue un archivo .py

---
+++ Trabajo Practicos/Para prueba de Git.py
@@ -0,0 +1,7 @@
+
+
+print("Hola Mundo")
+input_text = input("Ingrese un texto: ")
+print("El texto ingresado es:", input_text)
+text_length = len(input_text)
+print("La longitud del texto ingresado es:", text_length)
